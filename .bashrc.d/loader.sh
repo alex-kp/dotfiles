430cca7afc652852f84148f8a4d8cb7c6de5d5f0
alex-kp <alex-kp@users.noreply.github.com>
2022-03-26 08:13:56 +0000

    Go back to NT for files

--- .bashrc.d/loader.sh
+++ .bashrc.d/loader.sh
@@ -1,3 +1,26 @@
+# loader.sh
+# Load the other scripts in this directory
+
+# Utility functions for adding things to $PATH in an idempotent way
+# from: https://superuser.com/questions/39751
+pathappend() {
+  for ARG in "$@"
+  do
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+        PATH="${PATH:+"$PATH:"}$ARG"
+    fi
+  done
+}
+pathprepend() {
+  for ((i=$#; i>0; i--)); 
+  do
+    ARG=${!i}
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+        PATH="$ARG${PATH:+":$PATH"}"
+    fi
+  done
+}
+
 bashrc_dir=${HOME}/.bashrc.d
 loader_filename=loader.sh
 if [ -d ${bashrc_dir} ]; then
