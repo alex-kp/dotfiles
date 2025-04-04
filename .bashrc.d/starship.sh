cc626bb5e133807207d72606ddbf655ea04cc015
Alex KP <alex.knight-percival@unipartrail.com>
2025-04-04 11:53:24 +0100

    Add window title

--- .bashrc.d/starship.sh
+++ .bashrc.d/starship.sh
@@ -1,1 +1,17 @@
+function set_win_title(){
+  local current_dir="$PWD"
+  local home_dir="$HOME"
+
+  if [[ "$current_dir" == "$home_dir" ]]; then
+    title="~"
+  elif [[ "$current_dir" == "$home_dir"/* ]]; then
+    title="~/${current_dir#"$home_dir/"}"
+  else
+    title="$current_dir"
+  fi
+
+  echo -ne "\033]0; ${USER}@${HOSTNAME}: ${title} \007"
+  # echo -ne "\033]0; ${USER}@${HOSTNAME}: $(basename "$PWD") \007"
+}
+starship_precmd_user_func="set_win_title"
 eval "$(starship init bash)"
